ccab19a60dabe5caccf7e8033e7e2c784a34ece7
Anna <kochurovaav@gmail.com>
2023-02-23 15:38:00 +0200

    Task01 is done

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random r = New Random();
+
+Console.CursorVisible = false;
+while(true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+}
