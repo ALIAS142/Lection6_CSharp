07a77bdad9a7a0a6ad48a2df32ef0e947500db1d
Anna <kochurovaav@gmail.com>
2023-03-08 14:20:48 +0200

    Task03 is done

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Тесты:
+
+// for (int i = 1, j = 1; i < 10; i++, j++)
+//     Console.WriteLine($"{i * j}");
+
+
+//     int i = -1;
+// do
+// {
+//     Console.WriteLine(i);
+//     i--;
+// }
+// while (i > 0);
+
+
+// Таблица умножения:
+
+for (int i = 1; i < 10; i++)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        Console.Write($"{i * j} \t");
+    }
+    Console.WriteLine();
+}
