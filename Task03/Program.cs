7a27c522f92b888003d36786dc33550af657724f
Anna <kochurovaav@gmail.com>
2023-03-23 19:08:56 +0200

    task03 is modified

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -15,11 +15,37 @@
 
 // Таблица умножения:
 
-for (int i = 1; i < 10; i++)
+// for (int i = 1; i < 10; i++)
+// {
+//     for (int j = 1; j < 10; j++)
+//     {
+//         Console.Write($"{i * j} \t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Bootcamp3. Big O Notation
+// Таблица умножения
+
+int n = Convert.ToInt32(Console.ReadLine());
+int [ , ] matrix = new int [n, n];
+
+for (int i = 0; i < n; i++)
 {
-    for (int j = 1; j < 10; j++)
+    for (int j = i; j < n; j++)
     {
-        Console.Write($"{i * j} \t");
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write("   ");
     }
     Console.WriteLine();
 }
